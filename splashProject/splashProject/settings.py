98f7de6f3583c997cb6b204c06403d63f19058bb
park bo young <584934293@qq.com>
2021-07-30 10:07:50 +0800

    settings.py

--- splashProject/splashProject/settings.py
+++ splashProject/splashProject/settings.py
@@ -39,7 +39,8 @@ ROBOTSTXT_OBEY = False
 DEFAULT_REQUEST_HEADERS = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Accept-Language': 'en',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55',
 }
 
 # Enable or disable spider middlewares
