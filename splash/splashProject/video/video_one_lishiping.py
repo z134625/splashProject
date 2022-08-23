1d077d38b8060e508b45e2d2cb9369efdbe77366
z134625 <584934293@qq.com>
2022-08-23 16:44:25 +0800

    add os

--- splash/splashProject/video/video_one_lishiping.py
+++ splash/splashProject/video/video_one_lishiping.py
@@ -3,7 +3,7 @@
 # @Date  : 2021-07-31
 import requests
 import re
-
+import os
 response = requests.get('https://pic.netbian.com/4kmeinv/')
 response.encoding = 'gbk'
 res = re.findall('<li>.*?img src="(.*?)" alt=.*?<b>(.*?)</b>.*?', response.text, re.S)
